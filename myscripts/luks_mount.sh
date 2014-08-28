2e01551530a3325ff7af88dd13eaff4ca526274a
labunix <labunix@ibm-amddebian.myhome.local>
2014-08-29 02:49:41 +0900

    20140829_024915 backup

--- myscripts/luks_mount.sh
+++ myscripts/luks_mount.sh
@@ -7,6 +7,15 @@ fi
 
 USER=labunix
 DEST=/home/${USER}/mydata
+LUKS_HEADER_BACKUP=luks.header_`env LANG=C date '+%Y%m%d_%H%M%S'`
+
+cryptsetup luksHeaderBackup /home/luks.img --header-backup-file ${LUKS_HEADER_BACKUP}
+
+test -f ${LUKS_HEADER_BACKUP} && \
+  openssl enc -e -aes-256-ofb -in "${LUKS_HEADER_BACKUP}" -out /home/${USER}/mydata/luks.header.enc
+rm -f "${LUKS_HEADER_BACKUP}"
+
+chown -R ${USER}:${USER} /home/${USER}/myscripts
 test -b /dev/dm-0 || cryptsetup luksOpen /home/luks.img luks
 test -d "${DEST}" || mkdir ${DEST}
 mount | grep luks >/dev/null || mount /dev/mapper/luks ${DEST}
--- myscripts/luks_unmount.sh
+++ myscripts/luks_unmount.sh
@@ -5,17 +5,10 @@ if [ `id -u` -ne "0" ] ;then
   exit 1
 fi
 
-LUKS_HEADER_BACKUP=luks.header_`env LANG=C date '+%Y%m%d_%H%M%S'`
-
 USER=labunix
 DEST=/home/${USER}/mydata
 umount $DEST
 cryptsetup luksClose luks
-cryptsetup luksHeaderBackup /home/luks.img --header-backup-file ${LUKS_HEADER_BACKUP}
-test -f ${LUKS_HEADER_BACKUP} && \
-  openssl enc -e -aes-256-ofb -in "${LUKS_HEADER_BACKUP}" -out /home/${USER}/myscripts/luks.header.enc
-rm -f "${LUKS_HEADER_BACKUP}"
-chown -R ${USER}:${USER} /home/${USER}/myscripts
 
 mount | grep luks
 
